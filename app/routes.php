d1f80ab08012a0faf3498945ac5e2fe86201d3e7
Dele O <delomos@gmail.com>
2013-03-20 07:06:15 -0400

    added base mailchimp API integration

--- app/routes.php
+++ app/routes.php
@@ -12,3 +12,24 @@
 */
 
 Route::get('/', 'HomeController@showWelcome');
+
+
+Route::get('subscribe', function () {
+
+    $apikey = '6e34cfe71e585e702cd74a2e167b1526-us5';
+    $mc_api = new \Mailchimp\MCAPI($apikey);
+    $list_id = $mc_api->lists()['data'][0]['id']; //Coming  Soon Subscribers
+    $email = 'dele@geleyi.com';
+    
+
+    $data  = ['FNAME'=>'testUser', 'LNAME' => 'testUser'];
+    
+    // this is a double opt-in process, users have to 
+    // confirm from their emails before being signed up
+    if( $list_id ){
+    	$return_data = $mc_api->listSubscribe($list_id, $email, $data, $email_type = 'html');
+    }
+
+    dd($return_data);
+
+});
