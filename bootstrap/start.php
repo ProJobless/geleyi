295743661307670eae4763feecf1dd49511ef4d0
Dele O <delomos@gmail.com>
2013-03-24 20:56:37 -0400

    [#45735653] bullet-proofing font-face

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,9 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-//	'local' => array('your-machine-name'),
+	'local' => array('*dev'),
+//    'staging' => array('staging.geleyi.dev'),
+    'production' => array('*geleyi.dev')
 
 ));
 
