99236b39f9488af6da74d2803d23c06b24e4db24
Dele O <delomos@gmail.com>
2013-03-24 21:54:30 -0400

    [#45735653] adding google analytics to landing page

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -8,6 +8,7 @@
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width"/>
     <title>Geleyi: African Fashion. Simple. Beautiful. Joyful.</title>
+    <meta name="description" content="Geleyi is an online marketplace and business incubator for African-inspired designs. We help designers grow their businesses and provide a brand new shopping experience for consumers to explore the wider range of Africa-inspired fashion. We partner with designers and other servicing firms to create a new market for African-inspired designs.">
     <link rel="stylesheet" href="build/css/normalize.css?v=1.0.0"/>
     <link rel="stylesheet" href="build/css/app.css?v=1.0.0"/>
     <script src="dependencies/custom.modernizr.js?v=1.0.0"></script>
@@ -29,5 +30,16 @@
 
 
 <script src="build/application.js"></script>
+<!-- GA Code -->
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-39559273-1', 'geleyi.com');
+    ga('send', 'pageview');
+
+</script>
 </body>
 </html>
