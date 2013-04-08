1538c22455d3bfa89247800c448a52e074b6d90c
Dele O <delomos@gmail.com>
2013-04-07 20:50:16 -0400

    updated paths, removed the names for the list, see: http://bit.ly/ZsVv48

--- app/routes.php
+++ app/routes.php
@@ -23,13 +23,15 @@ Route::get('subscribe', function () {
     $email = $_GET['email'];
     
 
-    $data  = ['FNAME'=>'testUser', 'LNAME' => 'testUser'];
+    $data  = ['FNAME'=>'', 'LNAME' => ''];
     
     // this is a double opt-in process, users have to 
     // confirm from their emails before being signed up
     //mailchimp returns a bool
     if( $list_id ){
     	$return_data = $mc_api->listSubscribe($list_id, $email, $data, $email_type = 'html');
+
+//        dd( $return_data );
     }
 
     return $return_data ? $email : false;
