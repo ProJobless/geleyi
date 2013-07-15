c01f76fc65722fd2649f0aafa1632f0ab5119278
Dele O <delomos@gmail.com>
2013-07-14 21:10:56 -0400

    drop SSL config

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -8,7 +8,10 @@
 Spree.config do |config|
   # Example:
   # Uncomment to override the default site name.
-  # config.site_name = "Spree Demo Site"
+  config.site_name = "Geleyi"
+  config.allow_ssl_in_production = false
 end
 
 Spree.user_class = "Spree::User"
+
+
